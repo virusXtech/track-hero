270234d29a12a66b354590a0c2185019c12487de
thisisfaisalhere <thisisfaisalhere@gmail.com>
2023-09-28 00:48:45 +0530

    feat: redesign

---
+++ src/components/Navbar.vue
@@ -0,0 +1,90 @@
+<template>
+  <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <a href="" class="flex items-center">
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">TrackHERO</span>
+      </a>
+      <div class="flex md:order-2">
+        <button
+          id="theme-toggle"
+          type="button"
+          class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+          @click="toggleTheme"
+        >
+          <svg
+            ref="darkIcon"
+            :class="[darkTheme ? 'hidden' : '', 'w-5', 'h-5']"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+          </svg>
+          <svg
+            ref="lightIcon"
+            :class="[!darkTheme ? 'hidden' : '', , 'w-5', 'h-5']"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Navbar',
+}
+</script>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const lightIcon = ref<HTMLOrSVGElement | null>(null)
+const darkIcon = ref<HTMLOrSVGElement | null>(null)
+const darkTheme = ref(true)
+
+const toggleTheme = () => {
+  // if set via local storage previously
+  if (localStorage.getItem('color-theme')) {
+    if (localStorage.getItem('color-theme') === 'light') {
+      document.documentElement.classList.add('dark')
+      localStorage.setItem('color-theme', 'dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+      localStorage.setItem('color-theme', 'light')
+    }
+
+    // if NOT set via local storage previously
+  } else {
+    if (document.documentElement.classList.contains('dark')) {
+      document.documentElement.classList.remove('dark')
+      localStorage.setItem('color-theme', 'light')
+    } else {
+      document.documentElement.classList.add('dark')
+      localStorage.setItem('color-theme', 'dark')
+    }
+  }
+}
+
+onMounted(() => {
+  if (
+    localStorage.getItem('color-theme') === 'dark' ||
+    (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+  ) {
+    document.documentElement.classList.add('dark')
+    darkTheme.value = true
+  } else {
+    document.documentElement.classList.remove('dark')
+    darkTheme.value = true
+  }
+})
+</script>
