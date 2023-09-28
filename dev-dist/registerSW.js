1059b4e8ea1d8bbdaa3240ad519ddc15143ea4cc
thisisfaisalhere <thisisfaisalhere@gmail.com>
2023-09-28 10:54:42 +0530

    feat: pwa enabled

---
+++ dev-dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/track-hero/dev-sw.js?dev-sw', { scope: '/track-hero/', type: 'classic' })
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   server: {
     port: 8000,
   },
